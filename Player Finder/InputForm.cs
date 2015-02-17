dfaeb2fdb6807d7842a4a8a9cf6061f71131573b
Matt <matthew.farra0@gmail.com>
2015-02-17 10:41:20 -0500

    Code for parsing user pages for the specified appID and number of hours added

--- Player Finder/InputForm.cs
+++ Player Finder/InputForm.cs
@@ -21,6 +21,11 @@ namespace Player_Finder
             InitializeComponent();
         }
 
+        private void InputForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
         private void SteamGroupLabel_Click_1(object sender, EventArgs e)
         {
 
@@ -72,21 +77,96 @@ namespace Player_Finder
         }
         private void SearchButton_Click(object sender, EventArgs e)
         {
+            string data = "";
             int userpage = 1;
-
+            bool diff = true;
             // This line takes the user's input into the group box and puts it into a string for the group's URL.
             string groupurl = "http://steamcommunity.com/groups/" + GroupBox.Text + "/memberslistxml/?xml=1&p=";
-            
-            
 
+
+            while (diff == true)
+            {
+                diff = false;
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(groupurl + userpage);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    Stream receiveStream = response.GetResponseStream();
+                    StreamReader readStream = null;
+
+                    if (response.CharacterSet == null)
+                    {
+                        readStream = new StreamReader(receiveStream);
+                    }
+                    else
+                    {
+                        readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                    }
+
+                    data = readStream.ReadToEnd();
+
+                    response.Close();
+                    readStream.Close();
+                }
+                userpage++;
+                int index = 0;
+                while (-1 != (index = data.IndexOf("<steamID64>")))
+                {
+                    string temp = data.Substring(index + 11, 17);
+                    if (idarray.Contains(temp))
+                    {
+
+                    }
+                    else
+                    {
+                        diff = true;
+                        idarray.Add(temp);
+                    }
+                    data = data.Substring(index + 28);
+                }
+            }
+            // Web request sent for each Steam ID in the array.
+            foreach (string id in idarray)
+            {
+                string gameurl = "http://steamcommunity.com/profiles/" + id + "/games/?xml=1";
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(gameurl);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    Stream receiveStream = response.GetResponseStream();
+                    StreamReader readStream = null;
+
+                    if (response.CharacterSet == null)
+                    {
+                        readStream = new StreamReader(receiveStream);
+                    }
+                    else
+                    {
+                        readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                    }
+
+                    data = readStream.ReadToEnd();
+
+                    response.Close();
+                    readStream.Close();
+                }
+                int index = 0;
+                // Index of the specified appID found here.  Then, the next time <hoursOnRecord> is seen, it will be for the correct appID.
+                while (-1 != (index = data.IndexOf("<appID>"+AppIDBox.Text+"</appID>")))
+                {
+                    data = data.Substring(index);
+                    index = data.IndexOf("<hoursOnRecord>");
+                    data = data.Substring(index + 15);
+                    index = data.IndexOf("<");
+                    double hours = Double.Parse(data.Substring(0, index));
+
+                }
+            }
             // Results window opened here.
             ResultsForm results = new ResultsForm();
             results.Show();
-        }
-
-        private void InputForm_Load(object sender, EventArgs e)
-        {
-
         }
     }
 }
